c705f68cf96954123a155eae5fe0ef73c1af514e
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-02-22 19:50:17 +0700

    chore: updating package-lock.json

--- libs/common/dtos/create-user.dto.ts
+++ libs/common/dtos/create-user.dto.ts
@@ -1,9 +1,8 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
-  readonly username!: string;
+  @IsEmail()
+  readonly email!: string;
 
   @IsString()
   @IsNotEmpty()
