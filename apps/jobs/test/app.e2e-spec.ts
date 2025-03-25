9b1ff20921f252baa0ea63b9fd284665790959f1
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-03-25 09:56:46 +0700

    chore: add husky package.

--- apps/jobs/test/app.e2e-spec.ts
+++ apps/jobs/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { JobsModule } from './../src/jobs.module';
 
 describe('JobsController (e2e)', () => {
--- apps/reports/test/app.e2e-spec.ts
+++ apps/reports/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { ReportsModule } from './../src/reports.module';
 
 describe('ReportsController (e2e)', () => {
